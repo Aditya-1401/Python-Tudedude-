5da4d7c238cdefc92cfebf5621aedaa56e73a748
Aditya-1401 <adityaalw001@gmail.com>
2025-06-26 10:14:36 +0530

    Delete Assignment 03 directory

--- Assignement 03/Task 2.py
+++
@@ -1,9 +0,0 @@
-#Task 2 --> Using the Math Module for Calculations
-import math
-number = float(input("Enter the number = "))
-sqrt_val = math.sqrt(number)
-log_val = math.log(number)
-sine_val = math.sin(number)
-print(f"Square Root of {number} = {sqrt_val}")
-print(f"Natural Logarithm of {number} = {log_val}")
-print(f"Sine of {number} = {sine_val}")
